1f5e29b61d03777a3b5b9b8d06335e6036a967c8
Aru <rm.arumugam.2000@gmail.com>
2022-06-24 17:18:11 +0800

    changed main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from viz import test
 
 import env as custom_simple_spread
 
-env = custom_simple_spread.parallel_env(N=2, local_ratio=0, shuffle=True)
+env = custom_simple_spread.parallel_env(N=3, local_ratio=0, shuffle=True)
 env = to_vec_env(env)
 
 policy_kwargs = dict(
@@ -17,9 +17,10 @@ policy_kwargs = dict(
     )
 )
 
-# ppo = PPO.load("models/model.zip")
-ppo = PPO("MultiInputPolicy", env, policy_kwargs=policy_kwargs, verbose=1, tensorboard_log=".")
-ppo.learn(1_000_000, tb_log_name="tb_logs/simple_spread_dict")
-ppo.save("models/model_2")
+ppo = PPO.load("models/model_2_me.zip")
+ppo.env = env
+# ppo = PPO("MultiInputPolicy", env, policy_kwargs=policy_kwargs, verbose=1, tensorboard_log=".")
+# ppo.learn(1_000_000, tb_log_name="tb_logs/me")
+# ppo.save("models/model_2")
 
 print(test(env, ppo, render=True))
