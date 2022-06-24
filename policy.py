3f7583442bff618c53d6ea3e3400dd0c12103fc9
Aru <rm.arumugam.2000@gmail.com>
2022-06-25 03:07:09 +0800

    Added customMEv2

--- policy.py
+++ policy.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from torch import nn
 
@@ -40,6 +41,60 @@ class CustomMeanEmbeddingsExtractor(BaseFeaturesExtractor):
                 encoded_tensor_list.append(extractor(observations[key]).sum(dim=-2))
             else:
                 encoded_tensor_list.append(extractor(observations[key]))
+
+        result = th.cat(encoded_tensor_list, dim=-1)
+        assert result.shape[-1] == self._features_dim
+        return result
+
+class CustomMeanEmbeddingsExtractorV2(BaseFeaturesExtractor):
+    def __init__(self, observation_space: gym.spaces.Dict, keys, mean_keys, embedding_size=16):
+        # TODO we do not know features-dim here before going over all the items, so put something there. This is dirty!
+        super().__init__(observation_space, features_dim=1)
+
+        extractors = {}
+        self.mean_keys = mean_keys
+        self.keys = keys
+
+        total_concat_size = 0
+        for key, subspace in observation_space.spaces.items():
+            # The observation key is a vector, flatten it if needed
+            extractors[key] = OrderedDict({
+                "linear":nn.Sequential(
+                    nn.Linear(
+                        subspace.shape[-1],
+                        embedding_size,
+                    ),
+                    nn.ReLU()
+                ),
+                "tanh":nn.Sequential(
+                    nn.Linear(
+                        subspace.shape[-1],
+                        embedding_size,
+                    ),
+                    nn.Tanh()
+                )
+            })
+            extractors[key] = nn.ModuleDict(extractors[key])
+
+            total_concat_size += 2*embedding_size
+
+        self.extractors = nn.ModuleDict(extractors)
+
+        # Update the features dim manually
+        self._features_dim = total_concat_size
+
+    def forward(self, observations) -> th.Tensor:
+        encoded_tensor_list = []
+
+        for key in self.keys:
+            extractors = self.extractors[key]
+            extracted = []
+            for extractor in extractors.values():
+                if key in self.mean_keys:
+                    extracted.append(extractor(observations[key]).sum(dim=-2))
+                else:
+                    extracted.append(extractor(observations[key]))
+            encoded_tensor_list.append(th.cat(extracted, dim=-1))
 
         result = th.cat(encoded_tensor_list, dim=-1)
         assert result.shape[-1] == self._features_dim
