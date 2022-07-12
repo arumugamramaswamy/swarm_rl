efcdec7d80890698225bc209bb83ab61a2a8b76d
Aru <rm.arumugam.2000@gmail.com>
2022-07-12 14:10:55 +0800

    Changed main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from stable_baselines3 import PPO
-from policy import CustomMeanEmbeddingsExtractor
+from policy import CustomAttentionMeanEmbeddingsExtractor
 
 from to_vec_env import to_vec_env
 from viz import test
@@ -10,17 +10,17 @@ env = custom_simple_spread.parallel_env(N=3, local_ratio=0, shuffle=True)
 env = to_vec_env(env)
 
 policy_kwargs = dict(
-    features_extractor_class=CustomMeanEmbeddingsExtractor,
+    features_extractor_class=CustomAttentionMeanEmbeddingsExtractor,
     features_extractor_kwargs=dict(
         keys=list(env.observation_space.keys()),
         mean_keys={"entity_pos", "other_pos", "comm"}
     )
 )
 
-ppo = PPO.load("models/model_2_me.zip")
-ppo.env = env
-# ppo = PPO("MultiInputPolicy", env, policy_kwargs=policy_kwargs, verbose=1, tensorboard_log=".")
-# ppo.learn(1_000_000, tb_log_name="tb_logs/me")
-# ppo.save("models/model_2")
+# ppo = PPO.load("models/model_2_me.zip")
+# ppo.env = env
+ppo = PPO("MultiInputPolicy", env, policy_kwargs=policy_kwargs, verbose=1, tensorboard_log=".")
+ppo.learn(1_000_000, tb_log_name="tb_logs/attention_embeddings")
+ppo.save("models/attn_1")
 
 print(test(env, ppo, render=True))
