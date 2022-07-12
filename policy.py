96c357eca33ab77d7ac9ee8525b5287a2c88315b
Aru <rm.arumugam.2000@gmail.com>
2022-07-12 14:09:03 +0800

    Added Attention mean embeddings extractor

--- policy.py
+++ policy.py
@@ -99,3 +99,63 @@ class CustomMeanEmbeddingsExtractorV2(BaseFeaturesExtractor):
         result = th.cat(encoded_tensor_list, dim=-1)
         assert result.shape[-1] == self._features_dim
         return result
+
+class CustomAttentionMeanEmbeddingsExtractor(BaseFeaturesExtractor):
+    def __init__(self, observation_space: gym.spaces.Dict, keys, mean_keys, embedding_size=16, num_heads=4):
+        # TODO we do not know features-dim here before going over all the items, so put something there. This is dirty!
+        super().__init__(observation_space, features_dim=1)
+
+        extractors = {}
+        self.mean_keys = mean_keys
+        self.keys = keys
+
+        total_concat_size = 0
+        for key, subspace in observation_space.spaces.items():
+            # The observation key is a vector, flatten it if needed
+            extractors[key] = nn.Sequential(
+                nn.Linear(
+                    subspace.shape[-1],
+                    embedding_size,
+                ),
+                nn.ReLU()
+            )
+            total_concat_size += embedding_size
+
+        self.extractors = nn.ModuleDict(extractors)
+
+        attn_heads = {}
+        for mean_key in mean_keys:
+            attn_heads[mean_key] = nn.MultiheadAttention(embedding_size, num_heads, batch_first=True)
+
+        self.attn_heads = nn.ModuleDict(attn_heads)
+        # Update the features dim manually
+        self._features_dim = total_concat_size
+        
+
+    def forward(self, observations) -> th.Tensor:
+        encoded_tensor_dict = {}
+
+        for key in self.keys:
+            extractor = self.extractors[key]
+            encoded_tensor_dict[key] = extractor(observations[key])
+
+
+        my_vel = encoded_tensor_dict["my_vel"]
+        assert len(my_vel.shape) == 2
+
+        weighted_dict = {}
+        for mean_key in self.mean_keys:
+            attn_head = self.attn_heads[mean_key]
+            query = th.reshape(my_vel, (my_vel.shape[0], 1, my_vel.shape[-1]))
+            weighted_dict[mean_key], _ = attn_head(query, encoded_tensor_dict[mean_key], encoded_tensor_dict[mean_key])
+            weighted_dict[mean_key] = weighted_dict[mean_key].squeeze(dim=-2)
+
+        encoded_tensor_dict.update(weighted_dict)
+
+        encoded_tensor_list = []
+        for key in self.keys:
+            encoded_tensor_list.append(encoded_tensor_dict[key])
+            
+        result = th.cat(encoded_tensor_list, dim=-1)
+        assert result.shape[-1] == self._features_dim
+        return result
