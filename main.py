1b674c3318eb300dd4f08c1d1256555e005fb52e
Aru <rm.arumugam.2000@gmail.com>
2022-07-13 10:19:00 +0800

    Added eval callback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import EvalCallback
 from policy import CustomAttentionMeanEmbeddingsExtractor
 
 from to_vec_env import to_vec_env
@@ -6,7 +7,8 @@ from viz import test
 
 import env as custom_simple_spread
 
-env = custom_simple_spread.parallel_env(N=3, local_ratio=0, shuffle=True)
+N = 3
+env = custom_simple_spread.parallel_env(N=N, local_ratio=0, shuffle=True)
 env = to_vec_env(env)
 
 policy_kwargs = dict(
@@ -17,10 +19,25 @@ policy_kwargs = dict(
     )
 )
 
+eval_env = custom_simple_spread.parallel_env(N=N, local_ratio=0, shuffle=True)
+eval_env = to_vec_env(eval_env)
+
+eval_callback_kwargs = {
+    "eval_env": eval_env,
+    "best_model_save_path": "eval",
+    "log_path": "eval",
+    "deterministic": True,
+    "eval_freq": 10000,
+    "n_eval_episodes": 100,
+    "render": False
+}
+
+eval_cb = EvalCallback(**eval_callback_kwargs)
+
 # ppo = PPO.load("models/model_2_me.zip")
 # ppo.env = env
-ppo = PPO("MultiInputPolicy", env, policy_kwargs=policy_kwargs, verbose=1, tensorboard_log=".")
-ppo.learn(1_000_000, tb_log_name="tb_logs/attention_embeddings")
-ppo.save("models/attn_1")
+ppo = PPO("MultiInputPolicy", env, policy_kwargs=policy_kwargs, verbose=1, tensorboard_log="tb_logs")
+ppo.learn(6_000_000, tb_log_name="attention_embeddings", callback=eval_cb)
+ppo.save("models/attn_2")
 
 print(test(env, ppo, render=True))
