ed38cb85af94541546326bf2419b5162606734bd
Aru <rm.arumugam.2000@gmail.com>
2022-07-22 01:01:41 +0800

    added config folder

--- run_experiment.py
+++ run_experiment.py
@@ -1,7 +1,7 @@
 from parse_config import parse_config
 from experimenter.core import Experiment
 
-ex = Experiment(allow_dirty=True)
+ex = Experiment()
 
 @ex.main
 def run_exp(cfg_node, experiment_dir):
